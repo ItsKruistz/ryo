3621cc4c8cc98d64fda03d02c0af9623ba3d319c
NotteShock <lekruistz@gmail.com>
2024-07-20 08:11:18 +0000

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
 <main>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <Button>Button</Button>
 </main>
