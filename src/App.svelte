b36d831e8896e100f618cc68b6218118e4b8c8e2
NotteShock <lekruistz@gmail.com>
2024-07-27 04:09:07 +0000

    i

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button";
-  import { Switch } from "$lib/components/ui/switch";
 </script>
 
 <main>
-  <h1 class="text-4xl font-bold lg:font-5xl">
-    Ryo
-  </h1>
-  <Switch />
-  <Button>
-    Button
-  </Button>
+  <Button>Button</Button>
 </main>
